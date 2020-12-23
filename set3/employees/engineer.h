59f6218318eed640a6aa3fa0fd782f8fc3103e9b
stepin104663 <69088408+stepin104663@users.noreply.github.com>
2020-12-23 06:15:29 +0530

    Update engineer.h

--- set3/employees/engineer.h
+++ set3/employees/engineer.h
@@ -7,9 +7,8 @@ class Engineer : public Employee {
   int m_projCode;
   public:
   Engineer(std::string id, std::string name, double salary,int exp,int code);
-  void payroll();
-  void appraisal();
-  void display();
+  double payroll();
+  double appraisal();
 };
 
 #endif
